9a50beb5c398c4159ccec5378da9f5018274b88b
Danny Duong <danny.duong4.00@outlook.com>
2022-07-19 14:20:09 -0600

    Refactor to use classes

--- main.js
+++ main.js
@@ -1,92 +1,108 @@
-let myLibrary = [];
+class Library {
+    #books = [];
 
-myLibrary.push(new Book("Harper Lee", "To Kill a Mockingbird", 281, true));
-myLibrary.push(new Book("The Great Gatsby", "F. Scott Fitzgerald", 208, true));
-myLibrary.push(new Book("One Hundred Years of Solitude", "Gabriel García Márquez", 448, false));
-myLibrary.push(new Book("A Passage to India", "E.M. Forster", 368, false));
-myLibrary.push(new Book("Invisible Man", "Ralph Ellison", 581, true));
+    constructor() {
+        this.#books.push(new Book("Harper Lee", "To Kill a Mockingbird", 281, true));
+        this.#books.push(new Book("The Great Gatsby", "F. Scott Fitzgerald", 208, true));
+        this.#books.push(new Book("One Hundred Years of Solitude", "Gabriel García Márquez", 448, false));
+        this.#books.push(new Book("A Passage to India", "E.M. Forster", 368, false));
+        this.#books.push(new Book("Invisible Man", "Ralph Ellison", 581, true));
+    }
 
-displayLibrary();
+    display() {
+        let booksNode = document.querySelector(".books");
+        booksNode.innerHTML = "";
 
-function Book(author, title, pageCount, read) {
-    this.author = author;
-    this.title = title,
-    this.pageCount = pageCount;
-    this.read = read;
-}
+        for (let book of this.#books) {
+            let bookNode = document.createElement("div");
+            bookNode.classList.add("book", "box");
 
-function displayLibrary() {
-    let books = document.querySelector('.books');
-    books.innerHTML = '';
+            let information = document.createElement("div");
+            information.classList.add("information");
 
-    for (let book of myLibrary) {
-        let bookDiv = document.createElement('div');
-        bookDiv.classList.add('book', 'box');
+            for (let property in book) {
+                let label = document.createElement("h4");
+                label.textContent =
+                    property === "author"
+                        ? "Author"
+                        : property === "title"
+                            ? "Title"
+                            : property === "pageCount"
+                                ? "Page count"
+                                : "Read";
+                information.appendChild(label);
+                let value = document.createElement("p");
+                value.textContent = book[property] === true ? "Yes" : book[property] === false ? "No" : book[property];
+                information.appendChild(value);
+            }
 
-        let information = document.createElement('div');
-        information.classList.add('information');
+            bookNode.appendChild(information);
 
-        for (let property in book) {
-            let label = document.createElement('h4');
-            label.innerHTML =
-                property === 'author'? 'Author':
-                property === 'title'? 'Title':
-                property === 'pageCount'? 'Page count':
-                'Read';
-            information.appendChild(label);
-            let value = document.createElement('p');
-            value.innerHTML =
-                book[property] === true? 'Yes':
-                book[property] === false? 'No':
-                book[property];
-            information.appendChild(value);
+            let readButton = document.createElement("button");
+            readButton.classList.add("button");
+            readButton.textContent = "Toggle Read";
+            readButton.addEventListener("click", () => {
+                book.read = !book.read;
+                information.lastChild.innerHTML = book.read ? "Yes" : "No";
+            });
+            bookNode.appendChild(readButton);
+
+            let deleteButton = document.createElement("button");
+            deleteButton.classList.add("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", () => {
+                booksNode.removeChild(bookNode);
+            });
+            bookNode.appendChild(deleteButton);
+
+            booksNode.appendChild(bookNode);
         }
+    }
 
-        bookDiv.appendChild(information);
-        
-        let readButton = document.createElement('button');
-        readButton.classList.add('button');
-        readButton.innerHTML = 'Toggle Read';
-        readButton.addEventListener('click', () => {
-            book.read = !book.read;
-            information.lastChild.innerHTML = book.read? 'Yes': 'No';
-        })
-        bookDiv.appendChild(readButton);
-
-        let deleteButton = document.createElement('button');
-        deleteButton.classList.add('button');
-        deleteButton.innerHTML = 'Delete';
-        deleteButton.addEventListener('click', () => {
-            books.removeChild(bookDiv);
-        })
-        bookDiv.appendChild(deleteButton);
-
-        books.appendChild(bookDiv);
+    addBook(book) {
+        this.#books.push(book);
     }
 }
 
-let modal = document.querySelector('.modal');
+class Book {
+    author;
+    title;
+    pageCount;
+    read;
 
-let newBook = document.querySelector('.new-book');
-newBook.addEventListener('click', () => {
-    modal.style.display = "flex";
-    document.querySelectorAll('form > input').forEach((input) => input.value = '')
-})
+    constructor(author, title, pageCount, read) {
+        this.author = author;
+        this.title = title;
+        this.pageCount = pageCount;
+        this.read = read;
+    }
+}
 
-window.onclick = function (e) {
+const library = new Library();
+library.display();
+
+let modal = document.querySelector(".modal");
+modal.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.style.display = "none";
     }
-}
+});
 
-document.querySelector('form').addEventListener('submit', (e) => {
+let newBook = document.querySelector(".new-book");
+newBook.addEventListener("click", () => {
+    modal.style.display = "flex";
+    document.querySelectorAll("form > input").forEach((input) => (input.value = ""));
+});
+
+document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
-    let author = document.querySelector('.author').value;
-    let title = document.querySelector('.title').value;
-    let pageCount = document.querySelector('.pageCount').value;
-    let read = document.querySelector('.read').checked;
 
-    myLibrary.push(new Book(author, title, pageCount, read));
-    displayLibrary();
+    let author = document.querySelector(".author").value;
+    let title = document.querySelector(".title").value;
+    let pageCount = document.querySelector(".pageCount").value;
+    let read = document.querySelector(".read").checked;
+
+    library.addBook(new Book(author, title, pageCount, read));
+    library.display();
     modal.style.display = "none";
-})
+});
